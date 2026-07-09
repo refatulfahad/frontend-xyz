b1d08033699f0c446a7f970c7958b6cc3cf0d214
Md-Refatul-Fahad <refatm452@gmail.com>
2026-07-09 09:36:39 +0600

    fix: change keycloak url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
     production: true,
     apiBaseUrl: 'https://backend-xyz-bqexayewepepa3cz.southeastasia-01.azurewebsites.net/api',
-    keycloakBaseUrl: 'https://keycloak-xyz.ambitiousglacier-652cfb90.southeastasia.azurecontainerapps.io',
-    keycloakLoginUrl: 'https://keycloak-xyz.ambitiousglacier-652cfb90.southeastasia.azurecontainerapps.io',
+    keycloakBaseUrl: 'https://keycloak-xyz.whiteglacier-6bac99b7.southeastasia.azurecontainerapps.io',
+    keycloakLoginUrl: 'https://keycloak-xyz.whiteglacier-6bac99b7.southeastasia.azurecontainerapps.io',
     keycloakRealm: 'project-xyz',
     keycloakClientId: 'public-client',
     keycloakRedirectUri: 'https://mango-field-0cea64400.7.azurestaticapps.net/auth_redirect',
